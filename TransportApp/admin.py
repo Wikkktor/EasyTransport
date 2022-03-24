6980af374503dcb37e58efe8e44e2562713108f4
Wiktor Karaszewicz <wkaraszewicz@gmail.com>
2022-03-24 15:08:44 +0100

    Adding models to admin

--- TransportApp/admin.py
+++ TransportApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Car, Transport, Order, Driver
 
-# Register your models here.
+admin.site.register(Car)
+admin.site.register(Transport)
+admin.site.register(Order)
+admin.site.register(Driver)
