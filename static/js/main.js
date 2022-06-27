76aff56751ddd83cc30cca19d1da59ce4e2fafa3
Wiktor Karaszewicz <wkaraszewicz@gmail.com>
2022-06-27 16:16:47 +0200

    adding removing dark mode cookie

--- static/js/main.js
+++ static/js/main.js
@@ -4,7 +4,7 @@ window.onload = function () {
         sidebar = body.querySelector(".sidebar"),
         toggle = body.querySelector(".toggle"),
         modeSwitch = body.querySelector(".toggle-switch"),
-        modeText = body.querySelector(".mode-text")
+        modeText = body.querySelector(".mode-text");
 
 
     toggle.addEventListener("click", () => {
@@ -14,6 +14,38 @@ window.onload = function () {
 
     modeSwitch.addEventListener("click", () => {
         body.classList.toggle("dark");
+        initMap();
+        let cook = darkCookieGet("stdark");
+        if (body.classList[0] === "dark"){
+            if (cook == "")  {
+                setDarkCookie("stdark", 1, 30);
+            }
+        }
+        else {
+            if (cook != "")  {
+                document.cookie = "stdark=1;expires=Thu, 01 Jan 1970 00:00:01 GMT";
+            }
+        }
     })
 }
+    function setDarkCookie(cname, cvalue, exdays) {
+        const d = new Date();
+        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+        let expires = "expires=" + d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    }
 
+    function darkCookieGet(cname) {
+        let name = cname + "=";
+        let ca = document.cookie.split(';');
+        for (let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    }
