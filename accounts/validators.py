c017889a153a66013ad5b4d2312b6772d9902995
Wiktor Karaszewicz <wkaraszewicz@gmail.com>
2022-06-28 17:38:04 +0200

    Register validation

--- accounts/validators.py
+++ accounts/validators.py
@@ -1,10 +1,19 @@
 import re
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 
-def register_validator(email, password, password2):
+def register_validator(request, email, password, password2):
+    # TODO: zrobić walidacje rejestracji
     reg = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
     if re.match(reg, email):
+        check_user = User.objects.filter(email=email)
+        if check_user:
+            messages.add_message(request, messages.ERROR, "Podany email jest zajęty")
+            return False
         if password == password2:
             return True
-        return "Hasła się ze sobą nie zgadzają"
-    return "Nieprawidłowy email"
+        messages.add_message(request, messages.ERROR, "Podane hasła się różnią")
+        return False
+    messages.add_message(request, messages.ERROR, "Nie prawidłowy email")
+    return False
