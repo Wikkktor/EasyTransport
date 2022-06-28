6f7af57843e51b75282e58b70d5268d8b5546036
Wiktor Karaszewicz <wkaraszewicz@gmail.com>
2022-06-28 18:11:11 +0200

    Base for views from dashboard panel

--- TransportApp/views.py
+++ TransportApp/views.py
@@ -1,21 +1,22 @@
 from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
 from django.views import View
 from .geocode import get_location_lat_long, get_distance_time_values
 from .models import Order, Transport, Car, Driver
 
-class MainPage(View):
+
+class MainPage(LoginRequiredMixin, View):
     def get(self, request):
         return render(request, "main_page.html")
 
 
-class Add_order(View):
+class Add_order(LoginRequiredMixin, View):
     def get(self, request):
         return render(request, "add_order.html")
 
     def post(self, request):
         user = request.user
-        print(user)
         f_name = request.POST.get('first_name')
         l_name = request.POST.get('last_name')
         phone = request.POST.get('phone')
@@ -41,7 +42,29 @@ class Add_order(View):
         return redirect('main_page')
 
 
-
-class Orders_view(View):
+class Orders_view(LoginRequiredMixin, View):
     def get(self, request):
-        return render(request, 'orders_list.html')
+        user = request.user.id
+        orders = Order.objects.filter(user_id=user)
+        return render(request, 'orders_list.html', {'orders': orders})
+
+
+class Analysis(LoginRequiredMixin, View):
+    def get(self, request):
+        user = request.user.id
+        orders = Order.objects.filter(user_id=user)
+        # TODO: wykres na podstawie zamówień, łączna ilość km ile przejechał etc
+        return render(request, 'analysis.html', {'orders': orders})
+
+
+class Settings(LoginRequiredMixin, View):
+    def get(self, request):
+        user = request.user.id
+        return render(request, 'settings.html')
+
+
+class Contact(LoginRequiredMixin, View):
+    def get(self, request):
+        user = request.user.id
+        return render(request, 'help.html')
+
