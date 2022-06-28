6d7011af60a46ffb6fdb4fb884dc088635596e93
Wiktor Karaszewicz <wkaraszewicz@gmail.com>
2022-06-28 17:38:41 +0200

    Register checking all posibilities for error

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.contrib.auth import logout
 from django.shortcuts import render, redirect
 from django.views import View
@@ -5,6 +6,7 @@ from .validators import register_validator
 from django.contrib.auth.models import User
 from TransportApp.models import Account
 from .geolocation import get_location_lat_long
+
 
 class Register_view(View):
     def get(self, request):
@@ -18,13 +20,20 @@ class Register_view(View):
         password = request.POST.get("password")
         password2 = request.POST.get("password2")
         address = request.POST.get("address")
-        if register_validator(email, password, password2):
+        if register_validator(request, email, password, password2):
             user = User(username=email, email=email)
             user.set_password(password)
             user.save()
-            geolocation = get_location_lat_long(address)
+            try:
+                geolocation = get_location_lat_long(address)
+            except IndexError:
+                messages.add_message(request, messages.ERROR, 'Nie możemy znaleźc adresu Twojej firmy')
+                return redirect('register')
             account = Account(user=user, location=address, lat=geolocation[0], long=geolocation[1])
             account.save()
+            return redirect('main_page')
+        return redirect('register')
+
 
 class Login_view(View):
     def get(self, request):
