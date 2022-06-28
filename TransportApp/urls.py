6c4fc9f8cf0366bed1588122cf6ad1d1193a1975
Wiktor Karaszewicz <wkaraszewicz@gmail.com>
2022-06-28 18:10:51 +0200

    Adding path to views

--- TransportApp/urls.py
+++ TransportApp/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from TransportApp import views as v
 urlpatterns = [
     path('', v.MainPage.as_view(), name="main_page"),
-    path('dodaj-zamowienie/', v.Add_order.as_view(), name='add_order')
+    path('dodaj-zamowienie/', v.Add_order.as_view(), name='add_order'),
+    path('zamowienia/', v.Orders_view.as_view(), name='orders'),
+    path('analiza/', v.Analysis.as_view(), name='analysis'),
+    path('ustawienia/', v.Settings.as_view(), name='settings'),
+    path('kontakt-pomoc/', v.Contact.as_view(), name='contact')
 ]
