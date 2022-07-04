1d11c39aa77fb9216dc44ab07da578e3868a0b44
Wiktor Karaszewicz <wkaraszewicz@gmail.com>
2022-07-04 19:06:43 +0200

    Adding regex for filter page

--- TransportApp/urls.py
+++ TransportApp/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from TransportApp import views as v
 urlpatterns = [
     path('', v.MainPage.as_view(), name="main_page"),
     path('dodaj-zamowienie/', v.Add_order.as_view(), name='add_order'),
-    path('zamowienia/', v.Orders_view.as_view(), name='orders'),
+    re_path(r'^zamowienia/(?P<status>[0-4])?/?$', v.Orders_view.as_view(), name='orders'),
     path('analiza/', v.Analysis.as_view(), name='analysis'),
     path('ustawienia/', v.Settings.as_view(), name='settings'),
     path('kontakt-pomoc/', v.Contact.as_view(), name='contact')
