934fa4c960602cf30dbf0b5b707f7227225de5e8
Wiktor Karaszewicz <wkaraszewicz@gmail.com>
2022-07-05 22:43:19 +0200

    Analysis view querysets

--- TransportApp/views.py
+++ TransportApp/views.py
@@ -18,7 +18,7 @@ class MainPage(LoginRequiredMixin, View):
         not_done_orders = Order.objects.filter(user_id=user, status__lt=3)
         today_orders = Order.objects.filter(user_id=user, delivery_time__range=(today_min, today_max))
         return render(request, "main_page.html",
-                      {'orders': not_done_orders, 'today': today_orders, 'account':account})
+                      {'orders': not_done_orders, 'today': today_orders, 'account': account})
 
 
 class Add_order(LoginRequiredMixin, View):
@@ -50,7 +50,8 @@ class Add_order(LoginRequiredMixin, View):
                                  "Niestety nie możemy przekonwertować tego adresu, spróbuj dodać więcej danych")
             return redirect('add_order')
         order = Order.objects.create(user_id=user.id, client_name=f_name, client_surname=l_name, phone_number=phone,
-                                     delivery_address=delivery_address, delivery_time=delivery_dt, opis=info, lat=geocode[0],
+                                     delivery_address=delivery_address, delivery_time=delivery_dt, opis=info,
+                                     lat=geocode[0],
                                      lon=geocode[1], distance=distance, time=time_dist[1])
         order.save()
         return redirect('main_page')
@@ -70,9 +71,46 @@ class Analysis(LoginRequiredMixin, View):
     def get(self, request):
         user = request.user.id
         orders = Order.objects.filter(user_id=user)
-        drivers = Driver.objects.filter(user_id=user)
-        cars = Car.objects.filter(user_id=user)
-        context = {'drivers': drivers, 'cars': cars, 'orders': orders}
+        ranges = [
+            (orders.filter(distance__lte=20).count(), "0-20"),
+             (orders.filter(distance__lte=40, distance__gt=20).count(), "20-40"),
+              (orders.filter(distance__lte=80, distance__gt=40).count(), "40-80"),
+               (orders.filter(distance__lte=140, distance__gt=80).count(), "80-140"),
+                (orders.filter(distance__lte=220, distance__gt=140).count(), "140-220"),
+                 (orders.filter(distance__lte=320, distance__gt=220).count(), "220-320"),
+                  (orders.filter(distance__gt=320).count(), "+320"),
+        ]
+        drivers = Driver.objects.raw(f"""
+        
+          SELECT {user} 
+              AS id, "TransportApp_driver".name, SUM(distance::numeric), COUNT("TransportApp_order")
+            FROM "TransportApp_order"
+            JOIN "TransportApp_transport"
+              ON "TransportApp_transport".order_id= "TransportApp_order".id
+            JOIN "TransportApp_driver"
+              ON "TransportApp_driver".id = "TransportApp_transport".driver_id
+            JOIN "auth_user"
+              ON "auth_user".id = {user}
+        GROUP BY name
+        ORDER BY SUM(distance::numeric) DESC;
+
+        """)
+        cars = Car.objects.raw(f"""
+        
+        SELECT {user}
+            AS id, "TransportApp_car".name, SUM(distance::numeric), COUNT("TransportApp_order")
+          FROM "TransportApp_order"
+          JOIN "TransportApp_transport"
+            ON "TransportApp_transport".order_id= "TransportApp_order".id
+          JOIN "TransportApp_car"
+            ON "TransportApp_car".id = "TransportApp_transport".car_id
+          JOIN "auth_user"
+            ON auth_user.id = 1
+        GROUP BY name
+        ORDER BY SUM(distance::numeric) DESC;
+        
+        """)
+        context = {'drivers': drivers, 'cars': cars, 'ranges': ranges}
         return render(request, 'analysis.html', context)
 
 
